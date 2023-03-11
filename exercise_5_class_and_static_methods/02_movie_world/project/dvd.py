172d34e32cb679397feb96ec2ffe46df17f56b20
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-03-11 13:45:18 +0200

    Class and Static Methods - homework

---
+++ exercise_5_class_and_static_methods/02_movie_world/project/dvd.py
@@ -0,0 +1,21 @@
+from calendar import month_name
+
+
+class DVD:
+    def __init__(self, name: str, dvd_id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = dvd_id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, dvd_id: int, name: str, date: str, age_restriction: int):
+        day, month, year = [int(x) for x in date.split(".")]
+
+        return cls(name, dvd_id, year, month_name[month], age_restriction)
+
+    def __repr__(self):
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction " \
+               f"{self.age_restriction}. Status: {'rented' if self.is_rented else 'not rented'}"
---
+++ exercise_5_class_and_static_methods/02_movie_world/project/movie_world.py
@@ -0,0 +1,69 @@
+from typing import List
+from project.customer import Customer
+from project.dvd import DVD
+
+
+class MovieWorld:
+    def __init__(self, name: str):
+        self.name = name
+        self.customers: List[Customer] = []
+        self.dvds: List[DVD] = []
+
+    @staticmethod
+    def dvd_capacity() -> int:
+        return 15
+
+    @staticmethod
+    def customer_capacity() -> int:
+        return 10
+
+    def add_customer(self, customer: Customer) -> None:
+        if len(self.customers) < self.customer_capacity():
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd: DVD) -> None:
+        if len(self.dvds) < self.dvd_capacity():
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id: int, dvd_id: int):
+        customer = [c for c in self.customers if c.id == customer_id][0]
+        dvd = [d for d in self.dvds if d.id == dvd_id][0]
+
+        if dvd in customer.rented_dvds:
+            return f"{customer.name} has already rented {dvd.name}"
+
+        if dvd.is_rented:
+            return "DVD is already rented"
+
+        if customer.age < dvd.age_restriction:
+            return f"{customer.name} should be at least {dvd.age_restriction} to rent this movie"
+
+        customer.rented_dvds.append(dvd)
+        dvd.is_rented = True
+        return f"{customer.name} has successfully rented {dvd.name}"
+
+    def return_dvd(self, customer_id: int, dvd_id: int):
+        customer = [c for c in self.customers if c.id == customer_id][0]
+        dvd = [d for d in self.dvds if d.id == dvd_id][0]
+
+        if dvd in customer.rented_dvds:
+            dvd.is_rented = False
+            customer.rented_dvds.remove(dvd)
+            return f"{customer.name} has successfully returned {dvd.name}"
+
+        return f"{customer.name} does not have that DVD"
+
+    def __repr__(self):
+        result = []
+
+        for i in range(len(self.customers)):
+            result.append(self.customers[i].__repr__())
+
+        for i in range(len(self.dvds)):
+            result.append(self.dvds[i].__repr__())
+
+        return '\n'.join(result)
+
+
+
+
